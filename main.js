cb1604cc84404f292ae671386e41b1e82d700ea2
Elle H <itsellej@gmail.com>
2018-09-02 15:37:16 +0100

    Assign elements by ID to variables

---
+++ main.js
@@ -0,0 +1,15 @@
+const colourstart = document.getElementById("colour-start");
+const colourend = document.getElementById("colour-end");
+
+const ttop = document.getElementById("top");
+const topright = document.getElementById("top-right");
+const right = document.getElementById("right");
+const bottomright = document.getElementById("bottom-right");
+const bottom = document.getElementById("bottom");
+const bottomleft = document.getElementById("bottom-left");
+const left = document.getElementById("left");
+const topleft = document.getElementById("top-left");
+
+const button = document.getElementById("button");
+let gradientcode = document.getElementById("gradient-code")
+gradientcode.value = "working?"
