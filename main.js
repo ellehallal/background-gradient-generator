a8c249f997d83f797d1de0bb91cf96f845bc434b
Elle H <itsellej@gmail.com>
2018-09-02 20:01:51 +0100

    Add user gradient variable

--- main.js
+++ main.js
@@ -13,12 +13,14 @@ const bottomleft = document.getElementById("bottom-left");
 const left = document.getElementById("left");
 const topleft = document.getElementById("top-left");
 
-let direction = "top";
 
-//placeholders
+
+//placeholders / default values
 const button = document.getElementById("button");
 let gradientcode = document.getElementById("gradient-code")
-gradientcode.value = "working?"
+gradientcode.value = "background-image: linear-gradient(to top, #EE6E73, #F6C503)"
+let direction = "top";
+let usergradient = 'linear-gradient(to ' + direction + ', ' + colourstart.value + ', ' + colourend.value + ')';
 
 
 
@@ -42,6 +44,8 @@ function topclick(){
   console.log(direction)
 }
 
+// direction event listeners
 ttop.addEventListener("click", topclick);
 
-bodybg.style.backgroundImage = 'linear-gradient(to ' + direction + ', ' + colourstart.value + ', ' + colourend.value + ')';
+bodybg.style.backgroundImage = usergradient
+// gradientcode = usergradient
