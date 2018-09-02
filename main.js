4088fb02a395d3e830266fc9dfd5c3651529054a
Elle H <itsellej@gmail.com>
2018-09-02 16:48:27 +0100

    Add event listeners to input type colour fields. Add paragraphs with selected colour values

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const colourstart = document.getElementById("colour-start");
 const colourend = document.getElementById("colour-end");
+// const colourstartvalue = colourstart.value
+// const colourendvalue = colourstart.end
 
 const ttop = document.getElementById("top");
 const topright = document.getElementById("top-right");
@@ -13,3 +15,17 @@ const topleft = document.getElementById("top-left");
 const button = document.getElementById("button");
 let gradientcode = document.getElementById("gradient-code")
 gradientcode.value = "working?"
+
+
+function pcolourstart() {
+  let startvalue = document.getElementById("startvalue");
+  startvalue.innerHTML = colourstart.value;
+}
+
+function pcolourend() {
+  let endvalue = document.getElementById("endvalue");
+  endvalue.innerHTML = colourend.value;
+}
+
+colourstart.addEventListener("input", pcolourstart);
+colourend.addEventListener("input", pcolourend);
