2d3b7590489f8e4f7c994cf491deb9d8d456c369
Elle H <itsellej@gmail.com>
2018-09-02 19:45:40 +0100

    Add body height of 100vh. Add test gradient

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let bodybg = document.getElementById("bodybg");
 const colourstart = document.getElementById("colour-start");
 const colourend = document.getElementById("colour-end");
 let startvalue = document.getElementById("startvalue");
@@ -12,7 +13,7 @@ const bottomleft = document.getElementById("bottom-left");
 const left = document.getElementById("left");
 const topleft = document.getElementById("top-left");
 
-let selection = "";
+let direction = "";
 
 //placeholders
 const button = document.getElementById("button");
@@ -38,6 +39,9 @@ function topclick(){
   let pdirection = document.getElementById("pdirection")
   pdirection.innerHTML = "Selected gradient direction: to top";
   selection = "top";
+  console.log(selection)
 }
 
 ttop.addEventListener("click", topclick);
+
+bodybg.style.backgroundImage = 'linear-gradient(to top, #C0c0c0, #a8eb12)';
